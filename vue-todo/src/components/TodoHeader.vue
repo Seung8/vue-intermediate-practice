e9a57c2b5fb1ca49c7de8beb899dc8df939dfcdd
seung8 <lsy0914@naver.com>
2018-08-23 12:23:49 +0900

    페이지 공통 스타일링 및 Header 스타일링

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+/* scoped는 vue single file components에서 지원하는 속성으로 해당 컴포넌트 안에서만 동작하도록 설정하는 속성 */
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    /* rem은 font의 크기에 따라 조정 */
+    margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
+
