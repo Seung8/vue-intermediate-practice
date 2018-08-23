4d247663d0e0ad805006e56163740fd2e5f2b43c
seung8 <lsy0914@naver.com>
2018-08-23 12:15:22 +0900

    뷰 프로젝트 생성 및 기본 컴포넌트 생성

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- 기본적인 스캐폴딩 shortcut(Vetur) scf -->
+  <div class="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  },
+}
+</script>
+
+<style>
+
+</style>
